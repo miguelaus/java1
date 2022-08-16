8503da3c0551651ac2bff53fd9f12ba6301c7464
gitrodri <miguelaus@gmail.com>
2022-08-16 13:18:23 +0100

    creacion de repositorio y subida 1

---
+++ pruebajava.js
@@ -0,0 +1,79 @@
+	
+
+console.log("segunda prueba")
+
+// comentario con dos barritas para ordenar
+/*CSS*/
+
+
+//platos
+
+var plato1 = "patatas con cheddar"
+
+let plato2 = "hamburguesitas"
+
+let plato3 = "pasta"
+
+let plato4 = "pure"
+
+let plato5 = "churrasco"
+
+let plato6 = "ensalada"
+
+console.log(plato2)
+
+//Costos
+
+const costo1 = 10
+
+const costo2 = 5 
+
+const costo3 = 8
+
+const costo4 = 12
+
+const costo5 = 6
+
+const costo6 = 4
+
+//Mesas
+
+const mesa1 =  "mesa 1"
+
+const mesa2 =  "mesa 2"
+
+const mesa3 =  "mesa 3"
+
+const mesa4 =  "mesa 4"
+
+const mesa5 =  "mesa 5"
+
+const mesa6 =  "mesa 6"
+
+console.log(costo1)
+
+//pedidos
+
+console.log("El pedido es "+ plato1 +" con "+ plato2 +" y cuesta "+ costo1 + costo2)
+
+console.log(`El pedido es ${plato1} con ${plato3} y su costo es ${costo1 + costo3} pesos`)
+
+console.log(`El pedido de ${mesa1} es ${plato3} con ${plato6} y cuesta ${costo3 + costo6}`)
+
+// Promedios de horas trabajadas
+
+let mesasAtendidas = prompt("ingrese mesas atendidas hoy")
+
+let horasTrabajadas = prompt("ingrese horas trabajadas hoy")
+
+document.write(`${mesasAtendidas / horasTrabajadas}`)
+
+alert(" Ud ha atendido: " + mesasAtendidas / horasTrabajadas + " por hora.")
+
+
+
+
+
+
+
+
